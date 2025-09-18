0618c46baddb46e2d436516ecc88cdb08aa567a8
ash-abeer <83274161+ash-abeer@users.noreply.github.com>
2025-09-18 15:27:25 +0500

    declare read/write functions

--- components/cst3240/touchscreen/cst3240_touchscreen.h
+++ components/cst3240/touchscreen/cst3240_touchscreen.h
@@ -25,6 +25,9 @@ class CST3240Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevic
   /// @brief Perform the internal setup routine for the CST3240 touchscreen.
   void setup_internal_();
 
+  i2c::ErrorCode read_register16_(uint16_t reg, uint8_t *data, size_t len);
+  i2c::ErrorCode write_register16_(uint16_t reg, const uint8_t *data, size_t len);
+
   bool setup_done_{false};
 
   InternalGPIOPin *interrupt_pin_{nullptr};
